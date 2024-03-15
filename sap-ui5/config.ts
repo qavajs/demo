5c094a00e384dd8e586ecb4bfce80ef9757279ce
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2024-03-15 22:39:40 +0200

    added demo for SAP UI5 todo app

---
+++ sap-ui5/config.ts
@@ -0,0 +1,34 @@
+import Memory from './memory';
+import App from './page_object';
+
+export default {
+    paths: ['features/*.feature'],
+    require: [
+        'node_modules/@qavajs/steps-playwright/index.js'
+    ],
+    browser: {
+        logLevel: 'warn',
+        timeout: {
+            page: 5000
+        },
+        capabilities: {
+            browserName: 'chromium',
+            headless: false
+        }
+    },
+    formatOptions: {
+        console: {
+            showLogs: true
+        }
+    },
+    format: [
+        'json:report/report.json',
+        '@qavajs/html-formatter:report/report.html',
+        '@qavajs/console-formatter',
+        'junit:report/report.xml'
+    ],
+    memory: new Memory(),
+    pageObject: new App(),
+    parallel: 1,
+    defaultTimeout: 25000
+}
