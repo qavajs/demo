fd1b961ca94a678b441216aa8077f33ea9962f40
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2024-05-27 17:44:33 +0300

    updated playwright-runner demo

--- playwright-runner/config.ts
+++ playwright-runner/config.ts
@@ -3,15 +3,7 @@ import App from './page_object';
 
 export default {
     paths: ['features/**/*.feature'],
-    require: [
-        'step_definition/*.ts',
-        'node_modules/@qavajs/steps-playwright/playwright-runner.js',
-        'node_modules/@qavajs/steps-memory/index.js'
-    ],
+    require: [ 'node_modules/@qavajs/playwright/index.js' ],
     memory: new Memory(),
     pageObject: new App(),
-    browser: {
-        timeout: {},
-        screenshot: ['afterStep']
-    }
 }
