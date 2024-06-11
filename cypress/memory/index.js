98e2e2816b1b90023a8531606cb041eb96927a0c
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2024-06-11 14:22:18 +0300

    reworked cypress demo with qavajs adapter

---
+++ cypress/memory/index.js
@@ -0,0 +1,7 @@
+export default class Constants {
+    value1 = '42';
+    value2 = '42';
+    value3 = '12';
+
+    wikipediaUrl = 'https://www.wikipedia.org/';
+}
---
+++ cypress/support.js
@@ -0,0 +1,10 @@
+import defineQavajs from '@qavajs/cypress/defineQavajs';
+import '@qavajs/cypress';
+
+import PageObject from './page_object'; // path to qavajs page objects
+import Memory from './memory'; // path to qavajs memory
+
+defineQavajs({
+    pageObject: new PageObject(),
+    memory: new Memory()
+});
