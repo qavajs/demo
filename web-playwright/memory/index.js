ec09f398578226deaf591c70659ba368e9fa0c33
Alexander Galichenko <alexandr.galichenko@gmail.com>
2024-06-28 15:52:05 +0300

    update demo (#51)

--- web-playwright/memory/index.js
+++ web-playwright/memory/index.js
@@ -1,6 +1,6 @@
 class Constants {
     wikipediaUrl = 'https://www.wikipedia.org/';
-    todo = 'https://todomvc.com/examples/vanillajs/';
+    todo = 'https://todomvc.com/examples/javascript-es6/dist/';
 }
 
 module.exports = Constants;
--- web-wdio-esm/webdriver.js
+++ web-wdio-esm/webdriver.js
@@ -4,12 +4,16 @@ import App from './page_object/index.js';
 export default {
     paths: ['features/*.feature'],
     import: [
-        'node_modules/@qavajs/steps-wdio/index.js', 'step_definitions/*.js'
+        'node_modules/@qavajs/steps-wdio/index.js',
+        'step_definitions/*.js'
     ],
     browser: {
         logLevel: 'warn',
         capabilities: {
-            browserName: 'chrome'
+            browserName: 'chrome',
+            'goog:chromeOptions': {
+                args: ['--headless']
+            }
         }
     },
     format: [
