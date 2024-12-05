321d58d61643a480a8666aa4f59fb3d28d4d12be
Alexander Galichenko <alexandr.galichenko@gmail.com>
2024-12-05 16:16:11 +0200

    added playwright v2 example (#58)

---
+++ web-playwright-v2/config.ts
@@ -0,0 +1,32 @@
+import { Constants } from './memory';
+import { App } from './page_object'
+
+export default {
+    paths: ['features/*.feature'],
+    require: [
+        'node_modules/@qavajs/steps-playwright/index.js'
+    ],
+    browser: {
+        logLevel: 'warn',
+        timeout: {
+            page: 5000
+        },
+        capabilities: {
+            browserName: 'chromium'
+        }
+    },
+    formatOptions: {
+        console: {
+            showLogs: true
+        }
+    },
+    format: [
+        ['@qavajs/html-formatter', 'report/report.html'],
+        '@qavajs/console-formatter',
+        ['junit', 'report/report.xml']
+    ],
+    memory: new Constants(),
+    pageObject: App,
+    parallel: 1,
+    defaultTimeout: 25000
+}
