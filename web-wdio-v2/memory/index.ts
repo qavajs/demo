967e3095aace655544e07709535910e118b4123e
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2025-01-22 12:29:27 +0200

    added wdio v2 example

---
+++ web-wdio-v2/memory/index.ts
@@ -0,0 +1,8 @@
+export default class Constants {
+    value1 = '42';
+    value2 = '42';
+    value3 = '12';
+
+    wikipediaUrl = 'https://www.wikipedia.org/';
+    sauceDemoUrl = 'https://www.saucedemo.com/';
+}
---
+++ web-wdio-v2/page_object/index.ts
@@ -0,0 +1,48 @@
+import { locator } from '@qavajs/steps-wdio/po';
+import { Wikipedia } from './Wikipedia';
+import { WikipediaArticle } from './WikipediaArticle';
+
+export default class App {
+    Wikipedia = locator('.search-container').as(Wikipedia);
+    WikipediaArticle = locator( '#content').as(WikipediaArticle);
+
+    UsernameInput = locator('#user-name');
+    PasswordInput = locator('#password');
+    LoginButton = locator('#login-button');
+    ErrorMessage = locator('div.error');
+    AppLogo = locator('.app_logo');
+    Product = locator.template(text => `.inventory_item*=${text}`).as(Product);
+    CartButton = locator('a.shopping_cart_link');
+    CartBadge = locator('.shopping_cart_badge');
+    CartItems = locator('.cart_item');
+    CartItem = locator.template(text => `.cart_item*=${text}`).as(CartItem);
+    CheckoutButton = locator('#checkout');
+    CheckoutFirstName = locator('#first-name');
+    CheckoutLastName = locator('#last-name');
+    CheckoutPostalCode = locator('#postal-code');
+    ContinueButton = locator('#continue');
+    SummaryTotal = locator('.summary_total_label');
+    FinishButton = locator('#finish');
+    CompleteOrderHeader = locator('.complete-header');
+    MenuButton = locator('button#react-burger-menu-btn');
+    BurgerMenu = locator('.bm-menu').as(Menu);
+}
+
+class Product {
+    Price = locator('.inventory_item_price');
+    AddToCart = locator('[data-test^=add-to-cart]');
+}
+
+class CartItem {
+    Title = locator('.inventory_item_name');
+    Price = locator('.inventory_item_price');
+    Remove = locator('[data-test^=remove]');
+    UsernameInput = locator('#user-name');
+    PasswordInput = locator('#password');
+    LoginButton = locator('#login-button');
+    AppLogo = locator('.app_logo');
+}
+
+class Menu {
+    LogoutButton = locator('a#logout_sidebar_link');
+}
