0ecc80dc0456fac3b49edb6957db5e935bbd83e8
Alexander Galichenko <alexandr.galichenko@gmail.com>
2025-10-13 11:38:47 +0300

    added android form (#92)

--- mobile-native-v2/page_object/AndroidApp.ts
+++ mobile-native-v2/page_object/AndroidApp.ts
@@ -6,6 +6,7 @@ export class App {
     NavMenu = locator(UiAutomator2('className("android.view.ViewGroup").index(2)')).as(NavMenu);
     LoginForm = locator(UiAutomator2('className("android.widget.ScrollView").description("Login-screen")')).as(LoginForm);
     NotificationPopup = locator(UiAutomator2('className("android.widget.FrameLayout").resourceId("android:id/content")')).as(NotificationPopup);
+    Form = locator.as(Form);
 }
 
 class NavMenu {
@@ -21,4 +22,15 @@ class LoginForm {
 
 class NotificationPopup {
     Message = locator(UiAutomator2('className("android.widget.TextView").resourceId("android:id/message")'));
+}
+
+class Form {
+    InputField = locator('//android.widget.EditText[@content-desc="text-input"]');
+    YouHaveTyped = locator('//android.widget.TextView[@content-desc="input-text-result"]');
+    Switch = locator('//android.widget.Switch[@content-desc="switch"]');
+    SwitchOff = locator('//android.widget.Switch[@content-desc="Click to turn the switch ON"]');
+    SwitchOn = locator('//android.widget.Switch[@content-desc="Click to turn the switch OFF"]');
+    Dropdown = locator('//android.view.ViewGroup[@content-desc="Dropdown"]');
+    DropdownOption = locator.template(text => `//android.widget.CheckedTextView[@text="${text}"]`);
+    Button = locator('//android.widget.TextView[@text="Active"]');
 }
