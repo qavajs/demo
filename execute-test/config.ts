850ac996a4f41021ec273e3ab9002ca2603fb4c9
Alexander Galichenko <alexandr.galichenko@gmail.com>
2025-10-29 18:59:00 +0200

    added execute test example (#95)

---
+++ execute-test/config.ts
@@ -0,0 +1,51 @@
+import 'dotenv/config';
+import { Constants } from './memory';
+import { App } from './page_object'
+
+const defaultConfig = {
+    paths: ['features/*.feature'],
+    require: [
+        'node_modules/@qavajs/steps-playwright/index.js',
+        'support/index.ts'
+    ],
+    browser: {
+        logLevel: 'warn',
+        timeout: {
+            page: 5000
+        },
+        capabilities: {
+            browserName: 'chromium',
+            headless: false
+        }
+    },
+    formatOptions: {
+        console: {
+            showLogs: true
+        }
+    },
+    format: [
+        ['@qavajs/html-formatter', 'report/report.html'],
+        '@qavajs/console-formatter',
+        ['junit', 'report/report.xml']
+    ],
+    memory: new Constants(),
+    pageObject: App,
+    parallel: 1,
+    defaultTimeout: 25000
+}
+
+export const headless = {
+    ...defaultConfig,
+    browser: {
+        logLevel: 'warn',
+        timeout: {
+            page: 5000
+        },
+        capabilities: {
+            browserName: 'chromium',
+            headless: true
+        }
+    },
+}
+
+export default defaultConfig;
