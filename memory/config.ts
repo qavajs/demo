d2fa86d5c3ad428daace6c3e3a594c0cca00cbce
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2025-11-19 15:44:29 +0200

    updated memory example

---
+++ memory/config.ts
@@ -0,0 +1,20 @@
+import Memory from './memory';
+
+export default {
+    paths: ['features/*.feature'],
+    require: [
+        'node_modules/@qavajs/steps-memory/index.js'
+    ],
+    format: [
+        '@qavajs/html-formatter:report/report.html',
+        '@qavajs/console-formatter',
+        'junit:report/report.xml'
+    ],
+    formatOptions: {
+        console: {
+            showLogs: true
+        }
+    },
+    memory: new Memory(),
+    parallel: 2
+}
--- memory/memory/index.ts
+++ memory/memory/index.ts
@@ -1,15 +1,13 @@
-class Constants {
+export default class Constants {
     value1 = '42';
     value2 = '42';
     value3 = '12';
 
     wikipediaUrl = 'https://www.wikipedia.org/';
-    number = numberLike => parseFloat(numberLike);
-    boolean = booleanLike => {
+    number = (numberLike: string) => parseFloat(numberLike);
+    boolean = (booleanLike: string) => {
         if (booleanLike === 'true') return true
         if (booleanLike === 'false') return false
         throw Error('Passed value is not boolean-like')
     }
 }
-
-module.exports = Constants;
