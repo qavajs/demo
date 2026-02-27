7fe402b48575870521cc560f08e85f484e148440
Alexander Galichenko <alexandr.galichenko@gmail.com>
2026-02-27 21:25:44 +0300

    updated native demo (#106)

--- playwright-wdio-runner/steps/native/index.ts
+++ playwright-wdio-runner/steps/native/index.ts
@@ -1,6 +1,6 @@
-import { Before, After, QavajsPlaywrightWdioWorld } from '@qavajs/playwright-wdio';
+import { Before, QavajsPlaywrightWdioWorld } from '@qavajs/playwright-wdio';
 
-Before({name: 'launch app'}, async function(this: QavajsPlaywrightWdioWorld) {
+Before({ name: 'launch app' }, async function(this: QavajsPlaywrightWdioWorld) {
     await this.driver.terminateApp('org.reactjs.native.example.wdiodemoapp');
     await this.driver.activateApp('org.reactjs.native.example.wdiodemoapp');
 });
